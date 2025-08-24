b2620eb1a2161f2d16e55ae58d276c7fd1a432f1
Israel <isra.viei255@gmail.com>
2025-08-24 00:50:31 -0300

    removendo arquivos excedentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { Route, Routes } from "react-router";
-
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
-import Dashboard from "./pages/Dashboard/Dashboard";
-import Places from "./pages/Places/Places";
 import RegisterPlace from "./pages/RegisterPlace/RegisterPlace";
-
 
 import './App.css'
 
@@ -15,8 +11,6 @@ function App() {
       <Routes>
         <Route path="/" Component={Login} />
         <Route path="/criar-conta" Component={Register} />
-        <Route path="/dashboard" Component={Dashboard} />
-        <Route path="/locais" Component={Places} />
         <Route path="/locais/novo" Component={RegisterPlace} />
       </Routes>
     </>
